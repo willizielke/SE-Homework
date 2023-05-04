3964614d65e7b98248e054b984c8fcb2cfc67fa6
Willi Zielke <willi.zielke@allegion.com>
2023-05-04 15:54:35 +0200

    use polymorphism

--- src/main/MovieStore/Rental.java
+++ src/main/MovieStore/Rental.java
@@ -3,12 +3,10 @@ package main.MovieStore;
 class Rental {
     private Movie movie;
     private int daysRented;
-
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
-
     public int getDaysRented() {
         return daysRented;
     }
@@ -17,30 +15,11 @@ class Rental {
         return movie;
     }
 
-    public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    public double getCharge(){
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
-            return 2;
-        }
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
